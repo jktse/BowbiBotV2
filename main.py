681984303a2a54140bd8ca5543a5c0e8a7d109e2
jktse <jktse@sfu.ca>
2022-04-27 13:22:09 -0700

    Creating the basic calls to turn on the bot

---
+++ main.py
@@ -0,0 +1,22 @@
+import os
+import discord
+from dotenv import load_dotenv
+
+
+load_dotenv()
+BOT_TOKEN = os.getenv('BOT_TOKEN')
+
+client = discord.Client()
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord!')
+
+class Application():
+    client.run(BOT_TOKEN)
+    
+
+def main():
+    app = Application()
+
+if __name__ == '__main__':
+    main()
