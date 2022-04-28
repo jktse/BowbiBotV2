7910fa9bae742cf539d384068b0226c09e2f0c97
jktse <21jackytse@gmail.com>
2022-04-28 11:39:10 -0700

    Migrating to nextcord

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
-import discord
-from discord.ext import commands
+import nextcord
+from nextcord.ext import commands
 from dotenv import load_dotenv
 
 # Load the Bot Token, this is important as it tells Discord API who we are
@@ -8,17 +8,20 @@ load_dotenv()
 BOT_TOKEN = os.getenv('BOT_TOKEN')
 
 # Intents are used to determine what the bot is allowed to do
-intents = discord.Intents.default()
+intents = nextcord.Intents.default()
 # bot is going to be the main callback that will await commands or instructions
 bot = commands.Bot(command_prefix='!', intents=intents)
 
-bot.load_extension("help") # Path to the file, instead of using a slash use a period
+bot.load_extension("help") # Path to the file remember that use dot to replace slashes and if there are none it automatically assumes .py
+
+for folder in os.listdir("modules"):
+    if os.path.exists(os.path.join("modules", folder, "cog.py")):
+        bot.load_extension(f"modules.{folder}.cog")
 
 class Application():
-    # This tells the application to boot up
+    # This tells the bot to boot up
     bot.run(BOT_TOKEN)
     
-
 def main():
     app = Application()
 
---
+++ modules/roll/cog.py
@@ -0,0 +1,31 @@
+# This file is mainly to contain the code for a random dice roll.
+import nextcord
+from nextcord.ext import commands
+
+class Roll(commands.Cog, name="Roll"):
+    """Recieves roll commands"""
+
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+
+    @commands.command()
+    async def roll(self, ctx: commands.Context):
+        """ Rolls a given amount of dice in the form _d_
+
+        Example: !roll 2d6
+        This is asking for 2 dice rolls of a 6 sided dice
+        """
+        await ctx.send("something")
+
+    @commands.command()
+    async def choose(self, ctx: commands.Context, *args):       # *args for an unlimited number of inputs, all put into an array
+        """ Chooses a random item from the given input, if the item is mutiple words used " "
+
+        Example: !choose First Second "Third Option"
+        One of the above inputs will be choosen, "Third Option will be designated as one word
+        """
+        await ctx.send("something")
+
+
+def setup(bot: commands.Bot):
+    bot.add_cog(Roll(bot))
