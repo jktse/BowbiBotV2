720d72e21d78e4a3f56be0cd3dbb97e9dd875cd0
jktse <21jackytse@gmail.com>
2022-04-28 18:38:19 -0700

    Refactor the comments

--- modules/help/help_command.py
+++ modules/help/help_command.py
@@ -1,10 +1,10 @@
 # This file contains the code to modify nextcord (discord.py) built in help commands
 # Code references from: https://gist.github.com/InterStella0/b78488fb28cadf279dfd3164b9f0cf96#brief
-
 # We expect 3 kinds of input from the user
 # !help (contains not argument we need to output all the cogs(groups of commands), and commands (found in each grouping of cogs)) -> send_bot_help
 # !help Roll (contains a cog which is a group of commands (also the class name) will display all the commands under this group) -> send_cog_help
 # !help roll (contains a command within a cog, display information only about the command)  -> send_command_help
+# If the user submits a regular !help command we add some interactivivity, I might consider adding it to cogs to help filter the commands as well.
 
 from multiprocessing.sharedctypes import Value
 from typing import Optional, Set
@@ -14,29 +14,36 @@ from nextcord import Embed
 import nextcord
 
 class HelpDropdown(nextcord.ui.Select):
+    # When we initialize for the help dropdown, might get replaced for pagination
     def __init__(self, help_command: "MyHelpCommand", options: list[nextcord.SelectOption]):
         super().__init__(placeholder="Choose a category...", min_values=1, max_values=1, options=options)
         self._help_command = help_command
 
+    # We wait for interaction made on the dropdown, if there is an interaction we update the help message for the user
+    # We update the embed information, self.values[0] is part of the drop down and we have the cog's label set at values[0]
+    # If there is no change nothing changes otherwise we get all the commands maped to the cog
     async def callback(self, interaction: nextcord.Interaction):
         embed = (
             await self._help_command.cog_help_embed(self._help_command.context.bot.get_cog(self.values[0]))
             if self.values[0] != self.options[0].value
             else await self._help_command.bot_help_embed(self._help_command.get_bot_mapping())
-
         )
         await interaction.response.edit_message(embed=embed)
 
 class HelpView(nextcord.ui.View):
+    # This is used to generate the dropdown that we see. We pass in the information about our help command
+    # The list of options the downdown will have as well as the timeout for the view
     def __init__(self, help_command: "MyHelpCommand", options: list[nextcord.SelectOption], *, timeout: Optional[float] = 120.0):
         super().__init__(timeout=timeout)
         self.add_item(HelpDropdown(help_command, options))
         self._help_command = help_command
 
+    # When the timeout has been reached for the help dropdown it will terminate kill everything. Thus the need to rewrite the last known help
     async def on_timeout(self) -> None:
         self.clear_items()
         await self._help_command.response.edit(view=self)
     
+    # This is meant to verify the one who invoked the help call is the one who can play with the dropdown
     async def interaction_check(self, interaction: nextcord.Interaction) -> bool:
         return self._help_command.context.author == interaction.user
 
@@ -44,10 +51,11 @@ class HelpView(nextcord.ui.View):
 
 
 class MyHelpCommand(commands.MinimalHelpCommand):
+    # Returns how the command should be formated in a string
     def get_command_signature(self, command):
-        # clean_prefix is the prefix we need, qualified name is the command name, signature is how you call the command
         return f"{self.context.clean_prefix} {command.qualified_name} {command.signature}"
 
+    # Returns the list of cogs which the user can see (Commands are filtered based on the user's permision)
     async def _cog_select_option(self) -> list[nextcord.SelectOption]:
         options: list[nextcord.SelectOption] = []
         options.append(nextcord.SelectOption(
@@ -61,6 +69,7 @@ class MyHelpCommand(commands.MinimalHelpCommand):
             if not filtered:
                 continue
             emoji = getattr(cog, "COG_EMOJI", None)
+            # Note description for SelectOption cannot be longer than 100 characters
             options.append(nextcord.SelectOption(
                 label = cog.qualified_name if cog else "No Category",
                 emoji=emoji,
@@ -68,7 +77,12 @@ class MyHelpCommand(commands.MinimalHelpCommand):
                 ))
         return options
 
-    # This helps us format the help message into an embeded object which looks nicer when the bot prints a message
+    # Returns an Embed object that will contain all the information we want to display
+    # Embeds contains 2 main parts the Title (instantiated at the beginning) and a fields (The body of the embed)
+    # We break the field adding into 2 parts set of commands vs. mapping
+    # If we are calling from the COG and want the set of commands then command_set is true
+    # If we are calling from the home (!help) then we need all the cogs called a mapping.
+    # In both cases we need pull the command information out however the level of detail is different
     async def _help_embed(
         self, title: str, description: Optional[str] = None, mapping: Optional[dict] = None,
         command_set: Optional[Set[commands.Command]] = None
@@ -111,22 +125,24 @@ class MyHelpCommand(commands.MinimalHelpCommand):
                 embed.add_field(name=cog_label, value=value)
         return embed
 
+    # This is called when !help is called, this returns an Embed object created from _help_embed
     async def bot_help_embed(self, mapping: dict) -> Embed:
-        # Returns Embed
         return await self._help_embed(
             title="Bot Commands",
             description=self.context.bot.description,
             mapping = mapping
         )
 
-    # This is the return statement when user types !help with not parameters (Thus we need to return all the commands and cogs)
+    # This is called when ever the user invoke !help with not arguments. Therefore we generate the dropdown
     async def send_bot_help(self, mapping: dict):
         # Generate the embed for this call.
         embed = await self.bot_help_embed(mapping)
-        # Send the embed to where the user messaged for the help
+        # Get the list of Cogs the users can see (options in terms of things seen on the dropdown)
         options = await self._cog_select_option()
+        # Send the embed to where the user messaged for the help
         self.response = await self.get_destination().send(embed=embed, view=HelpView(self, options))
 
+    # This is called when the user invokes !help [command]. We print the description of the command.
     async def send_command_help(self, command: commands.Command):
         # Generate the embed for commands
         # command.help will check for the comments under the command
@@ -138,6 +154,7 @@ class MyHelpCommand(commands.MinimalHelpCommand):
         )
         await self.get_destination().send(embed=embed)
 
+    # This returns the cogs information interms of an embed, we also add in the emoji to the top as a title
     async def cog_help_embed(self, cog: commands.Cog) -> Embed:
         emoji = getattr(cog, "COG_EMOJI", None)
         return await self._help_embed(
@@ -146,7 +163,7 @@ class MyHelpCommand(commands.MinimalHelpCommand):
             command_set=cog.get_commands()
         )
     
+    # This is called when the user invoke !help [COG]. We output all the cog commands to the user.
     async def send_cog_help(self, cog: commands.Command):
-        # Go throught the cog and output all the commands within the set
         embed = await self.cog_help_embed(cog)
         await self.get_destination().send(embed=embed)
